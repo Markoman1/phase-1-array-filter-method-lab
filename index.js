75d1257743976d10929e134b03062e6fa3b8fcb4
Mark <mgalex12@icloud.com>
2022-05-12 11:59:21 -0400

    commit tree d2511bd476ce30a6db88252f9e701205d1947147

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(drivers, name) {
+   const matching = (drivers.filter(driver => driver.toLowerCase() === name.toLowerCase()))
+    // console.log(matching)
+    return matching
+}
+
+
+
+function fuzzyMatch(arrayOfDrivers, firstLetters) {
+    const length = firstLetters.length
+    console.log(length)
+    const lettersMatch = arrayOfDrivers.filter(driver => driver.slice(0, length)=== firstLetters)
+    return lettersMatch        
+}  
+
+
+
+
+
+
+
+function matchName(arrayOfObjects, string) {
+     
+    return arrayOfObjects.filter((driver)=>driver.name === string)
+    
+}
